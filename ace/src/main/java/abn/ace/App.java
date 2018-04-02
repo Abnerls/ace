07b50457452124165079b45610522e646278da4d
Abnerls <abnerls@163.com>
2018-04-02 20:37:07 +0800

    git eclipse

---
+++ ace/src/main/java/abn/ace/App.java
@@ -0,0 +1,13 @@
+package abn.ace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
